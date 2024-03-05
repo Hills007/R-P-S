8d64c8187c9e5c50ec7e32dd107d748489e7e69a
Hills007 <toorkhan2000@yahoo.com>
2024-03-05 12:47:30 +0000

    Declare constants in js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Declare constants for DOM
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("p-score");
+const computerScore = document.getElementById("c-score");
+const playerImg = document.getElementById("p-image");
+const computerImg = document.getElementById("c-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
