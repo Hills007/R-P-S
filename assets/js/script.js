c5a1b0c0e856506a4dfa3395dda4acba7ea8f901
Hills007 <toorkhan2000@yahoo.com>
2024-03-05 12:49:37 +0000

    Add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ const playerImg = document.getElementById("p-image");
 const computerImg = document.getElementById("c-image");
 const messages = document.getElementById("messages");
 const choices = ["rock", "paper", "scissors"];
+
+
+// Add Event listeners
+for (let button of document.getElementsByClassName("control")) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
